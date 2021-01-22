725761655aaff63cb59864c2f0bbd2e8478112ee
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-22 17:40:59 +0530

    Validate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+                   length: { minimum: 4, maximum: 20 }
+  validates :email, presence: true, length: { maximum: 50 },
+                    uniqueness: { case_sensitive: false }
 end
