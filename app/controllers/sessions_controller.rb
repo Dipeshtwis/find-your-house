a1033b506b62e999a23b08e3fd6c1c2be1c563b8
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-22 19:14:36 +0530

    Add user login and logout feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  include CurrentuserConcern
+
   def create
     user = User.find_by(email: params["user"]["email"])
                .try(:authenticate, params["user"]["password"])
@@ -13,5 +15,26 @@ class SessionsController < ApplicationController
     else
       render json: { status: 401 }
     end
+  end
+
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+      	logged_in: false
+      }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: {
+      status: 200,
+      logged_out: true
+    }
   end
 end
