27c344421b7c3a48459b0ef2dfba3ccf969320c0
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-04-27 20:37:52 +0530

    Add more site to allow cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,4 +10,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true
   end
+
+  allow do
+    origins "https://homepointrdeep.netlify.app/"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    credentials: true
+  end
 end
