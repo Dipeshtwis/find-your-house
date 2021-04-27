3acd1faa2dab82567bba739111f0b4e8e8be9ad2
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-04-27 20:56:39 +0530

    Fix cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,7 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://homepointrdeep.netlify.app/"
+    origins "https://homepointrdeep.netlify.app"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true
   end
